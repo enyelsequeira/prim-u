e5f88b4dcdd71a7fe58a9536396ba3d10a770681
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-06-06 21:46:30 +0800

    adding contentful to the app and removing warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
+import { createClient } from "contentful";
 import Head from "next/head";
 import Example from "../components/example/example";
 import BaseLayout from "../layouts/base";
 
-export default function Home() {
+export default function Home({ data, services, faqQuestions, partnersQuestions, bigCards }) {
+  console.log(data);
+
+  console.log("+++++++++");
+  console.log(services);
+  console.log("=========");
+  console.log(faqQuestions);
+  console.log("=====pquestions=====");
+  console.log(partnersQuestions);
+  console.log("=====big cards======");
+  console.log(bigCards);
   return (
     <BaseLayout>
       <Head>
@@ -10,6 +21,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Example />
+      {services.map((card) => {
+        return (
+          <div key={card.fields.title}>
+            <p>{card.fields.title}</p>
+          </div>
+        );
+      })}
     </BaseLayout>
   );
+}
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: "udvzxn7no754",
+    accessToken: "6CYnQJMm1J9PqAIfK0-i1c0h-n8krHGJfNdBOTnVoxU",
+  });
+  const res = await client.getEntries();
+  const services = [];
+  const faqQuestions = [];
+  const partnersQuestions = [];
+  const bigCards = [];
+  // this needs to be changed to make things more readable
+  // pay attention to this, you should have all the data that you need
+  // beauty products needs to be added to the array as well
+  res.items.forEach((entry) => {
+    if (entry.fields.image) {
+      services.push(entry);
+    } else if (entry.fields.faqTitle) {
+      faqQuestions.push(entry);
+    } else if (entry.fields.partnersQuestions) {
+      partnersQuestions.push(entry);
+    } else if (entry.fields.bigCardsTitle) {
+      bigCards.push(entry);
+    }
+  });
+
+  return {
+    props: {
+      data: res.items,
+      services,
+      faqQuestions,
+      partnersQuestions,
+      bigCards,
+    },
+  };
 }
