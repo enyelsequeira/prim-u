f5e30e87f93e7665b84b6d3247b3685ff47e185d
Moddoo <>
2021-06-08 07:28:17 -0500

    css/variables

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,21 +1,23 @@
 import { useState } from 'react'
-import { Nav, LogoContainer, LogoIcon, LogoTitle, NavLinks, NavLink, NavIcon } from './nav.styles.js'
+import { Nav, NavContainer, LogoContainer, LogoIcon, LogoTitle, NavLinks, NavLink, NavIcon } from './nav.styles.js'
 
 function NavBar() {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <Nav isOpen={isOpen}>
-            <LogoContainer>
-                <LogoIcon />
-                <LogoTitle>PRIM–U</LogoTitle>
-            </LogoContainer>
-            <NavIcon onClick={() => setIsOpen(!isOpen)} />
-            <NavLinks>
-                <NavLink href='#'>MAKE A BOOKING</NavLink>
-                <NavLink href='#'>WORK WITH US</NavLink>
-                <NavLink href='#'>FAQ</NavLink>
-            </NavLinks>
+            <NavContainer>
+                <LogoContainer href='#'>
+                    <LogoIcon />
+                    <LogoTitle>PRIM–U</LogoTitle>
+                </LogoContainer>
+                <NavIcon onClick={() => setIsOpen(!isOpen)} />
+                <NavLinks>
+                    <NavLink href='#'>MAKE A BOOKING</NavLink>
+                    <NavLink href='#'>WORK WITH US</NavLink>
+                    <NavLink href='#'>FAQ</NavLink>
+                </NavLinks>
+            </NavContainer>
         </Nav>
     )
 }
